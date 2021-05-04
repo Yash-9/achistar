6f24e07e9a570c1565695e036281b857279167d5
Yash-9 <80215154+Yash-9@users.noreply.github.com>
2021-05-04 22:57:58 +0530

    Create Achistarwelcome.java

---
+++ Achistarwelcome.java
@@ -0,0 +1,6 @@
+public class AchiStar {
+    public static void main(String[] args) {
+        System.out.println("Hello, Customer!");
+        System.out.println("AchiStar Welcomes You"):
+    }
+}
