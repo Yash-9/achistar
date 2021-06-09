b4817ca722f350f507b6c194c52277e5933038f7
Yash <80215154+Yash-9@users.noreply.github.com>
2021-06-09 20:52:09 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -2,6 +2,7 @@ public class p1{
     public static void main () {
      System.out.print("Welcome to achistar");
      System.out.print("Achistar.com");
+     System.out.print("Help");
         
     }
 }
